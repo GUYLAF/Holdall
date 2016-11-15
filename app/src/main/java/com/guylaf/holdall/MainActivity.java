d5540a4a282b0e1157ec93bb90e02945bec0bc57
GUY LAFORET <GUY LAFORET>
2016-11-15 15:59:18 +0100

    Picasso

--- app/src/main/java/com/guylaf/holdall/MainActivity.java
+++ app/src/main/java/com/guylaf/holdall/MainActivity.java
@@ -1,6 +1,7 @@
 package com.guylaf.holdall;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"test1 ok",Toast.LENGTH_SHORT).show();
             }
         });
-
+        Button button2 = (Button) findViewById(R.id.test2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PicassoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/guylaf/holdall/PicassoActivity.java
@@ -0,0 +1,18 @@
+package com.guylaf.holdall;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class PicassoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picasso);
+
+    }
+}
