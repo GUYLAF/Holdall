b17bb14fc474214bc3f42b4d44a62e525ddc4e1d
GUY LAFORET <GUY LAFORET>
2016-11-15 16:38:07 +0100

    Image

--- app/src/main/java/com/guylaf/holdall/PicassoActivity.java
+++ app/src/main/java/com/guylaf/holdall/PicassoActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class PicassoActivity extends AppCompatActivity {
 
@@ -13,6 +16,10 @@ public class PicassoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picasso);
+        String url = "http://manosque-tennis.asptt.com/files/2016/01/tennis.jpg";
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        Picasso.with(this).load(url).into(imageView);
 
     }
 }
+
