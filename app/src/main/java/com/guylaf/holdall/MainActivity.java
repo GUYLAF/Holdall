a6af3fddd811c5dc2137ef8401f8218c55e9fc66
GUY LAFORET <GUY LAFORET>
2016-11-15 14:28:07 +0100

    UPDATE Bouton test1

--- app/src/main/java/com/guylaf/holdall/MainActivity.java
+++ app/src/main/java/com/guylaf/holdall/MainActivity.java
@@ -1,9 +1,9 @@
 package com.guylaf.holdall;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.TextureView;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.text);
-        textView.setText("Bonjour");
+        Button button = (Button) findViewById(R.id.test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
