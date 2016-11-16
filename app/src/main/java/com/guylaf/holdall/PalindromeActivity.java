817f43228522bad90caad2c45cb2815e69adbb25
GUY LAFORET <GUY LAFORET>
2016-11-16 14:26:17 +0100

    PAL Fab

--- app/src/main/java/com/guylaf/holdall/PalindromeActivity.java
+++ app/src/main/java/com/guylaf/holdall/PalindromeActivity.java
@@ -1,6 +1,8 @@
 package com.guylaf.holdall;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,18 +19,19 @@ public class PalindromeActivity extends AppCompatActivity {
         final EditText editText = (EditText) findViewById(R.id.edit_text);
         final TextView textView = (TextView) findViewById(R.id.text_view);
         final Button button = (Button) findViewById(R.id.button_ok);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(PalindromeActivity.this,editText.getText(), Toast.LENGTH_SHORT).show();
                 Palindrome p = new Palindrome();
-                String t = p.Inverse(editText.getText().toString());
+                //String t = p.Inverse(editText.getText().toString());
                 textView.setText(p.Inverse(editText.getText().toString()));
                 String pal = editText.getText().toString();
-                if (p.Palindrome(pal)==true){
-
-
-
+                if (p.Palindrome(pal)) {
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_green_dark)));
+                }else {
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_red_dark)));
                 }
             }
         });
